ff4d7c68bc63527d4d0d71f7a9546e6ec31bc4cf
Sammy Ageil <sammy.ageil@outlook.com>
2024-08-26 17:36:20 -0400

    chore: use locallowercase key in event.key

--- custom.js
+++ custom.js
@@ -1,35 +1,35 @@
 let blurdiv = null;
 function restore() {
-    blurdiv = document.getElementById('blurdiv');
-    if (blurdiv != null) {
-        blurdiv.removeAttribute('class');
-    }
+  blurdiv = document.getElementById('blurdiv');
+  if (blurdiv != null) {
+    blurdiv.removeAttribute('class');
+  }
 }
 document.addEventListener("DOMContentLoaded", function () {
-    document.addEventListener("keyup", function (event) {
-        if (event.key === "Enter") {
-            restore();
+  document.addEventListener("keyup", function (event) {
+    if (event.key === "Enter") {
+      restore();
+    }
+  });
+  document.addEventListener('keydown', function (event) {
+    if (event.key === 'Escape' || event.key === 'Esc') {
+      restore();
+    }
+    if ((event.metaKey || event.ctrlKey) && event.key.toLocaleLowerCase() === 'p') {
+      event.preventDefault();
+      const widget = document.querySelector('.quick-input-widget show-file-icons');
+      if (widget === null) {
+        const workbench = document.querySelector(".monaco-workbench");
+        if (workbench != null) {
+          blurdiv = document.getElementById('blurdiv');
+          if (blurdiv == null) {
+            blurdiv = document.createElement("div");
+            workbench.appendChild(blurdiv);
+            blurdiv.setAttribute('Id', 'blurdiv');
+          }
+          blurdiv.setAttribute('class', 'blurdiv');
         }
-    });
-    document.addEventListener('keydown', function (event) {
-        if (event.key === 'Escape' || event.key === 'Esc') {
-            restore();
-        }
-        if ((event.metaKey || event.ctrlKey) && event.key === 'p') {
-            event.preventDefault();
-            const widget = document.querySelector('.quick-input-widget show-file-icons');
-            if (widget === null) {
-                const workbench = document.querySelector(".monaco-workbench");
-                if (workbench != null) {
-                    blurdiv = document.getElementById('blurdiv');
-                    if (blurdiv == null) {
-                        blurdiv = document.createElement("div");
-                        workbench.appendChild(blurdiv);
-                        blurdiv.setAttribute('Id', 'blurdiv');
-                    }
-                    blurdiv.setAttribute('class', 'blurdiv');
-                }
-            }
-        }
-    }, true);
+      }
+    }
+  }, true);
 });
